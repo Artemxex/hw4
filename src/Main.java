2832409285a554c35528923654aae73d6308424a
Artemxex <Artem.363636@gmail.com>
2022-04-26 15:46:42 +0300

    hw4

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-t
+        //Задание 1
+        System.out.println("Задание 1 ");
+
+        int i = 0;
+        while ( i < 10 ) {
+            i = i + 1;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (; i > 0; i = i - 1) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+
+        //Задание 2
+        System.out.println("Задание 2 ");
+
+        int friday = 1;
+        while (friday < 31) {
+            System.out.println("Сегодня пятница "+ friday + " число");
+            friday = friday + 7;
+
+            }
+
+
+        //Задание 3
+        System.out.println("Задание 3 ");
+
+        int comet = 0;
+        while (comet < 2122) {
+            if (comet > 1822) {
+                System.out.println(comet);
+            }
+            comet = comet + 79;
+
+        }
+        }
+
+
 
 
 
     }
-}
